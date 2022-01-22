a8ca7fa6bdf04459ec89263e0c141ae9cd55cd5b
Benjamin Amos <benjamin.amos11@gmail.com>
2022-01-22 19:11:17 +0000

    feat(nui): convert WaypointCreationScreen to NUI

--- engine/src/main/java/org/destinationsol/game/screens/GameScreens.java
+++ engine/src/main/java/org/destinationsol/game/screens/GameScreens.java
@@ -21,6 +21,7 @@ import org.destinationsol.game.context.Context;
 import org.destinationsol.ui.SolLayouts;
 import org.destinationsol.ui.nui.screens.InventoryScreen;
 import org.destinationsol.ui.nui.screens.MapScreen;
+import org.destinationsol.ui.nui.screens.WaypointCreationScreen;
 
 import javax.inject.Inject;
 
@@ -46,7 +47,7 @@ public class GameScreens {
         menuScreen = new MenuScreen(layouts.menuLayout, cmp.getOptions());
         inventoryScreen = (org.destinationsol.ui.nui.screens.InventoryScreen) cmp.getNuiManager().createScreen("engine:inventoryScreen");
         talkScreen = new TalkScreen(layouts.menuLayout, cmp.getOptions());
-        waypointCreationScreen = new WaypointCreationScreen(layouts.menuLayout, cmp.getOptions(), mapScreen);
+        waypointCreationScreen = (WaypointCreationScreen) cmp.getNuiManager().createScreen("engine:waypointCreationScreen");
         consoleScreen = new ConsoleScreen(context.get(Console.class));
     }
 
